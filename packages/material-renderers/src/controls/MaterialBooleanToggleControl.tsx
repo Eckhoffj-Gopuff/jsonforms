89ce05ec06d3bf3b3ddb8bd7755ae7b8f0c1e630
Josh Eckhoff <josh.eckhoff@gopuff.com>
2025-02-07 09:23:12 -0600

    fix spacing

--- packages/material-renderers/src/controls/MaterialBooleanToggleControl.tsx
+++ packages/material-renderers/src/controls/MaterialBooleanToggleControl.tsx
@@ -23,7 +23,6 @@
   THE SOFTWARE.
 */
 import isEmpty from 'lodash/isEmpty';
-import merge from 'lodash/merge';
 import React from 'react';
 import {
   isBooleanControl,
@@ -32,11 +31,10 @@ import {
   ControlProps,
   optionIs,
   and,
-  isDescriptionHidden,
 } from '@jsonforms/core';
 import { withJsonFormsControlProps } from '@jsonforms/react';
-import { FormControlLabel, FormHelperText, Tooltip } from '@mui/material';
-import { MuiToggle } from '../mui-controls/MuiToggle';
+import { FormControlLabel, FormHelperText } from '@mui/material';
+import { MuiToggle } from '../mui-controls';
 
 export const MaterialBooleanToggleControl = ({
   data,
@@ -53,50 +51,13 @@ export const MaterialBooleanToggleControl = ({
   config,
   description,
 }: ControlProps) => {
-  const isValid = errors.length === 0;
-  const appliedUiSchemaOptions = merge({}, config, uischema.options);
-
-  const showDescription = !isDescriptionHidden(
-    visible,
-    description,
-    // Checkboxes do not receive focus until they are used, so
-    // we cannot rely on focus as criteria for showing descriptions.
-    // So we pass "false" to treat it as unfocused.
-    false,
-    appliedUiSchemaOptions.showUnfocusedDescription
-  );
-
-  const showTooltip =
-    !showDescription &&
-    !isDescriptionHidden(
-      visible,
-      description,
-      // Tooltips have their own focus handlers, so we do not need to rely
-      // on focus state here. So we pass 'true' to treat it as focused.
-      true,
-      // We also pass true here for showUnfocusedDescription since it should
-      // render regardless of that setting.
-      true
-    );
-
-  const firstFormHelperText = showDescription
-    ? description
-    : !isValid
-    ? errors
-    : null;
-  const secondFormHelperText = showDescription && !isValid ? errors : null;
-
   const descriptionIds = [];
-  const tooltipId = `${id}-tip`;
   const helpId1 = `${id}-help1`;
   const helpId2 = `${id}-help2`;
-  if (showTooltip) {
-    descriptionIds.push(tooltipId);
-  }
-  if (firstFormHelperText) {
+  if (description) {
     descriptionIds.push(helpId1);
   }
-  if (secondFormHelperText) {
+  if (errors) {
     descriptionIds.push(helpId2);
   }
   const ariaDescribedBy = descriptionIds.join(' ');
@@ -107,37 +68,39 @@ export const MaterialBooleanToggleControl = ({
 
   return (
     <>
-      <Tooltip id={tooltipId} title={showTooltip ? description : ''}>
-        <FormControlLabel
-          label={label}
-          id={id}
-          control={
-            <MuiToggle
-              id={`${id}-input`}
-              isValid={isEmpty(errors)}
-              data={data}
-              enabled={enabled}
-              visible={visible}
-              path={path}
-              uischema={uischema}
-              schema={schema}
-              rootSchema={rootSchema}
-              handleChange={handleChange}
-              errors={errors}
-              config={config}
-              inputProps={{
-                'aria-describedby': ariaDescribedBy,
-              }}
-            />
-          }
-        />
-      </Tooltip>
-      <FormHelperText id={helpId1} error={!isValid && !showDescription}>
-        {firstFormHelperText}
-      </FormHelperText>
-      <FormHelperText id={helpId2} error={!isValid}>
-        {secondFormHelperText}
-      </FormHelperText>
+      <FormControlLabel
+        label={label}
+        id={id}
+        control={
+          <MuiToggle
+            id={`${id}-input`}
+            isValid={isEmpty(errors)}
+            data={data}
+            enabled={enabled}
+            visible={visible}
+            path={path}
+            uischema={uischema}
+            schema={schema}
+            rootSchema={rootSchema}
+            handleChange={handleChange}
+            errors={errors}
+            config={config}
+            inputProps={{
+              'aria-describedby': ariaDescribedBy,
+            }}
+          />
+        }
+      />
+      {!!description && (
+        <FormHelperText id={helpId1} error={false}>
+          {description}
+        </FormHelperText>
+      )}
+      {!!errors && (
+        <FormHelperText id={helpId2} error={true}>
+          {errors}
+        </FormHelperText>
+      )}
     </>
   );
 };
